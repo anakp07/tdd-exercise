6242ffed863c30ada4936bf0dc52434a1778045c
Ana Parada <anakp07@gmail.com>
2020-09-29 14:14:16 -0700

    updated score/test files

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -2,6 +2,23 @@
 
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
-def blackjack_score(hand)
+  def blackjack_score(hand)
+    total = 0
+    hand.each do |card|
+      if !VALID_CARDS.any?(card)
+        raise ArgumentError
+      end
+      if card.is_a?(Integer)
+        total += card
+      elsif card.is_a?(String)
+        if card == "Jack" || card == "Queen" || card == "King"
+          total += 10
+        end
+      end
+    end
+    if total > 21
+      raise ArgumentError
+    end
+    return total
+  end
 
-end
